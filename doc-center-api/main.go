1fc1083dd1ccd26e3197b7cf1d3a3ddfe7cba619
Marlon Vinicius Batista <marlon.batista18@hotmail.com>
2023-03-11 18:35:30 -0300

    versão básica do projeto

---
+++ doc-center-api/main.go
@@ -0,0 +1,7 @@
+package main 
+
+func main(){
+	// criar rotas
+	//TODO: iniciar o servidor
+	//TODO: construir controllers
+}
---
+++ doc-center-web/src/cabecalho.js
@@ -0,0 +1,16 @@
+const Cabecalho = () => {
+    return (
+    <div>
+        <p>Lista de documentos</p>
+        <ul>
+            <li>RG</li>
+            <li>CPF</li>
+            <li>Passaporte</li>
+            <li>RG</li>
+            <li>RG</li>
+        </ul>
+    </div>
+    )
+}
+
+export default Cabecalho;
