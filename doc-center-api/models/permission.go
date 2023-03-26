e2b5e67e487c06229ab7213fcfb50030f18760b4
nicolasbalduino <nicolasbalduino@outlook.com>
2023-03-26 15:14:13 -0300

    traducao das struturas em go para ingles

--- doc-center-api/models/permission.go
+++ doc-center-api/models/permission.go
@@ -1,12 +1,18 @@
 package models
 
-import "time"
+import (
+	"time"
+)
 
-type Permissao struct {
-	Id        int64     `json:"id"`
+type Permission struct {
+	Id        int64     `json:"id" gorm:"primaryKey"`
 	FileId    int64     `json:"file"`
-	Ownerid   int64     `json:"owner-user"`
-	GuestId   int64     `json:"guest-user"`
+	OwnerId   int64     `json:"ownerUser"`
+	GuestId   int64     `json:"guestUser"`
 	Period    time.Time `json:"period"`
 	Permanent bool      `json:"permanent"`
+
+	File      File `gorm:"foreignKey:FileId"`
+	OwnerUser User `gorm:"foreignKey:OwnerId"`
+	GuestUser User `gorm:"foreignKey:GuestId"`
 }
