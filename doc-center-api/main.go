f95c587df73b609483fcadf5f2810a0659688f68
Marlon Vinicius Batista <marlon.batista18@hotmail.com>
2023-03-31 11:29:38 -0300

    Adicionado server para main

--- doc-center-api/main.go
+++ doc-center-api/main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
-	"doc-center-api/auth"
-	"doc-center-api/models"
+	"doc-center-api/api/auth"
+	"doc-center-api/api/routers"
+	"doc-center-api/domain/models"
+	"doc-center-api/infra/database"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	// "github.com/go-sql-driver/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
+var err error
+
 func main() {
+
+	database.DB, err = gorm.Open(mysql.Open(database.DdURL(database.BuildDBConfig())))
+
+	if err != nil {
+		fmt.Println("Status:", err)
+	}
+
+	defer database.CloseConn()
+	database.DB.AutoMigrate(&models.User{})
+
+	router := routers.ConfigRotas()
 
 	//Instancia a model de usuário
 	authService := auth.AuthService{
@@ -16,51 +35,51 @@ func main() {
 	}
 
 	//Instancia o router; função que manipula as rotas HTTP
-	router := gin.Default()
+	// router := gin.Default()
 
-	router.POST("/login", func(responseHttp *gin.Context) { //O responseHttp é a variável que vai armazenar os dados enviados pelo Http
+	// router.POST("/login", func(responseHttp *gin.Context) { //O responseHttp é a variável que vai armazenar os dados enviados pelo Http
 
-		//Cria uma struct consumindo os dados do
-		var loginData struct {
-			Username string `json:"username" binding:"required"`
-			Password string `json:"password" binding:"required"`
-		}
+	// 	//Cria uma struct consumindo os dados do
+	// 	var loginData struct {
+	// 		Username string `json:"username" binding:"required"`
+	// 		Password string `json:"password" binding:"required"`
+	// 	}
 
-		//Chama a função ShouldBindJSON que verifica se o loginData é válido
-		if erro := responseHttp.ShouldBindJSON(&loginData); erro != nil {
+	// 	//Chama a função ShouldBindJSON que verifica se o loginData é válido
+	// 	if erro := responseHttp.ShouldBindJSON(&loginData); erro != nil {
 
-			responseHttp.JSON(http.StatusBadRequest, gin.H{
-				"error": erro.Error(),
-			})
-			return
-		}
+	// 		responseHttp.JSON(http.StatusBadRequest, gin.H{
+	// 			"error": erro.Error(),
+	// 		})
+	// 		return
+	// 	}
 
-		//Chama a função CreateToken e gera o token do usuário
-		tokenString, erro := authService.CreateToken(loginData.Username, loginData.Password)
+	// 	//Chama a função CreateToken e gera o token do usuário
+	// 	tokenString, erro := authService.CreateToken(loginData.Username, loginData.Password)
 
-		//Se der erro no token
-		if erro != nil {
-			responseHttp.JSON(http.StatusUnauthorized, gin.H{
-				"error": erro.Error(),
-			})
-			return
-		}
+	// 	//Se der erro no token
+	// 	if erro != nil {
+	// 		responseHttp.JSON(http.StatusUnauthorized, gin.H{
+	// 			"error": erro.Error(),
+	// 		})
+	// 		return
+	// 	}
 
-		cookie := http.Cookie{
-			Name:     "token",
-			Value:    tokenString,
-			HttpOnly: true,
-			Secure:   true,
-			Path:     "/",
-		}
+	// 	cookie := http.Cookie{
+	// 		Name:     "token",
+	// 		Value:    tokenString,
+	// 		HttpOnly: true,
+	// 		Secure:   true,
+	// 		Path:     "/",
+	// 	}
 
-		//Insere o cookie com o token gerado
-		http.SetCookie(responseHttp.Writer, &cookie)
+	// 	//Insere o cookie com o token gerado
+	// 	http.SetCookie(responseHttp.Writer, &cookie)
 
-		//Retorna uma mensagem
-		responseHttp.JSON(http.StatusOK, gin.H{"message": "login successful"})
+	// 	//Retorna uma mensagem
+	// 	responseHttp.JSON(http.StatusOK, gin.H{"message": "login successful"})
 
-	})
+	// })
 
 	router.GET("/verify", func(c *gin.Context) {
 
