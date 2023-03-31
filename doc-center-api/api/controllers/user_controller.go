9952bc8c1c41ff884cf8909bf57d878a329ce48b
Marlon Vinicius Batista <marlon.batista18@hotmail.com>
2023-03-31 11:20:38 -0300

    Adicionado user_controller

---
+++ doc-center-api/api/controllers/user_controller.go
@@ -0,0 +1,75 @@
+package controllers
+
+import (
+	"doc-center-api/domain/handlers"
+	"doc-center-api/domain/models"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetAllUsers(c *gin.Context) {
+	var user []models.User
+	err := handlers.GetAllUsers(&user)
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+	} else {
+		c.JSON(http.StatusOK, user)
+	}
+}
+
+// CreateUser ... Create User
+func CreateUser(c *gin.Context) {
+	var user models.User
+	c.BindJSON(&user)
+	err := handlers.CreateUser(&user)
+	if err != nil {
+		fmt.Println(err.Error())
+		c.AbortWithStatus(http.StatusNotFound)
+	} else {
+		c.JSON(http.StatusOK, user)
+	}
+}
+
+func GetUserByID(c *gin.Context) {
+	id := c.Params.ByName("id")
+	var user models.User
+	err := handlers.GetUserByID(&user, id)
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+	} else {
+		c.JSON(http.StatusOK, user)
+	}
+}
+
+func UpdateUser(c *gin.Context) {
+	var user models.User
+	id := c.Params.ByName("id")
+	err := handlers.GetUserByID(&user, id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, user)
+	}
+	c.BindJSON(&user)
+	err = handlers.UpdateUser(&user, id)
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+	} else {
+		c.JSON(http.StatusOK, user)
+	}
+}
+
+func DeleteUser(c *gin.Context) {
+	var user models.User
+	id := c.Params.ByName("id")
+	err := handlers.DeleteUser(&user, id)
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+	} else {
+		c.JSON(http.StatusOK, gin.H{"id" + id: "is deleted"})
+	}
+}
+
+func Login(c *gin.Context) {
+    
+}
--- doc-center-api/controllers/user_controller.go
+++
@@ -1,6 +0,0 @@
-package controllers
-
-func TestUserController() {
-    // função de teste, que pode ser removida depois
-    println("Testando a controller de usuário...")
-}
