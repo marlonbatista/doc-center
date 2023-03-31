4f9e907c421311142c5e3752a6ae055b23e3525c
Marlon Vinicius Batista <marlon.batista18@hotmail.com>
2023-03-31 11:21:28 -0300

    movido pasta model

---
+++ doc-center-api/domain/models/user.go
@@ -0,0 +1,22 @@
+package models
+
+import "time"
+
+type User struct {
+	Id            int64  `json:"id" gorm:"primaryKey"`
+	Name          string `json:"name"`
+	NaturalPerson bool   `json:"naturalPerson"`
+	CPF           string `json:"cpf"`
+	// CPF           string    `json:"cpf" gorm:"check:check_cpf, naturalPerson = 1"`
+	BornDate time.Time `json:"bornDate"`
+	CNPJ     string    `json:"cnpj"`
+	// CNPJ          string    `json:"cnpj" gorm:"check:check_cnpj, naturalPerson = 1"`
+	CompanyName string `json:"companyName"`
+	TradingName string `json:"tradingName"`
+	Email       string `json:"email"`
+	Password    string `json:"password"`
+}
+
+func (b *User) User() string {
+	return "user"
+}
---
+++ doc-center-api/infra/database/database.go
@@ -0,0 +1,52 @@
+package database
+
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+type DBConfig struct {
+	Host     string
+	Port     int
+	User     string
+	DBName   string
+	Password string
+}
+
+func BuildDBConfig() *DBConfig {
+	dbConfig := DBConfig{
+		Host:     "localhost",
+		Port:     3306,
+		User:     "root",
+		Password: "125678",
+		DBName:   "DocCenterDB",
+	}
+	return &dbConfig
+}
+
+func DdURL(dbConfig *DBConfig) string {
+	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local",
+		dbConfig.User,
+		dbConfig.Password,
+		dbConfig.Host,
+		dbConfig.Port,
+		dbConfig.DBName,
+	)
+}
+
+func CloseConn() error {
+	config, err := DB.DB()
+	if err != nil {
+		return err
+	}
+
+	err = config.Close()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
