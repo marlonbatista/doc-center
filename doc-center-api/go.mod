7563b77c0bbe4eb920816c6971ffd02ad7fab906
Marlon Vinicius Batista <marlon.batista18@hotmail.com>
2023-04-28 19:47:27 -0300

    Rotas modificadas

--- doc-center-api/go.mod
+++ doc-center-api/go.mod
@@ -5,6 +5,8 @@ go 1.20
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.9.0
+	github.com/golang-jwt/jwt/v4 v4.5.0
+	golang.org/x/crypto v0.7.0
 )
 
 require (
@@ -33,7 +35,6 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	golang.org/x/arch v0.3.0 // indirect
-	golang.org/x/crypto v0.7.0 // indirect
 	golang.org/x/net v0.8.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
--- doc-center-api/main.go
+++ doc-center-api/main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"doc-center-api/api/auth"
-	"doc-center-api/api/routers"
+	"doc-center-api/api/routes"
 	"doc-center-api/domain/models"
 	"doc-center-api/infra/database"
 	"fmt"
-	"net/http"
+	"os"
 
-	"github.com/gin-gonic/gin"
 	// "github.com/go-sql-driver/mysql"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -27,92 +25,15 @@ func main() {
 	defer database.CloseConn()
 	database.DB.AutoMigrate(&models.User{})
 
-	router := routers.ConfigRotas()
+	port := os.Getenv("PORT")
 
-	//Instancia a model de usuário
-	authService := auth.AuthService{
-		Users: []models.User{},
+	if port == "" {
+		port = "8080"
 	}
 
-	//Instancia o router; função que manipula as rotas HTTP
-	// router := gin.Default()
-
-	// router.POST("/login", func(responseHttp *gin.Context) { //O responseHttp é a variável que vai armazenar os dados enviados pelo Http
-
-	// 	//Cria uma struct consumindo os dados do
-	// 	var loginData struct {
-	// 		Username string `json:"username" binding:"required"`
-	// 		Password string `json:"password" binding:"required"`
-	// 	}
-
-	// 	//Chama a função ShouldBindJSON que verifica se o loginData é válido
-	// 	if erro := responseHttp.ShouldBindJSON(&loginData); erro != nil {
-
-	// 		responseHttp.JSON(http.StatusBadRequest, gin.H{
-	// 			"error": erro.Error(),
-	// 		})
-	// 		return
-	// 	}
-
-	// 	//Chama a função CreateToken e gera o token do usuário
-	// 	tokenString, erro := authService.CreateToken(loginData.Username, loginData.Password)
-
-	// 	//Se der erro no token
-	// 	if erro != nil {
-	// 		responseHttp.JSON(http.StatusUnauthorized, gin.H{
-	// 			"error": erro.Error(),
-	// 		})
-	// 		return
-	// 	}
-
-	// 	cookie := http.Cookie{
-	// 		Name:     "token",
-	// 		Value:    tokenString,
-	// 		HttpOnly: true,
-	// 		Secure:   true,
-	// 		Path:     "/",
-	// 	}
-
-	// 	//Insere o cookie com o token gerado
-	// 	http.SetCookie(responseHttp.Writer, &cookie)
-
-	// 	//Retorna uma mensagem
-	// 	responseHttp.JSON(http.StatusOK, gin.H{"message": "login successful"})
-
-	// })
-
-	router.GET("/verify", func(c *gin.Context) {
-
-		//Busca o token nos cookies da requisição
-		cookie, erro := c.Cookie("token")
-
-		//Se der erro, retorna:
-		if erro != nil {
-			c.JSON(http.StatusUnauthorized, gin.H{
-				"message": "Token not found",
-			})
-			return
-		}
-
-		//Chama a função para decodificar o token
-		token, erro := authService.VerifyToken(cookie)
-
-		//Se der erro, retorna
-		if erro != nil {
-			c.JSON(http.StatusUnauthorized, gin.H{
-				"message": "Invalid token",
-			})
-			return
-		}
-
-		//Se der certo, retorna o nome do usuário
-		c.JSON(http.StatusUnauthorized, gin.H{
-			"message": token,
-		})
-
-	})
+	router := routes.ConfigRotas()
 
 	//Inicializa o framewrok de rotas
-	router.Run()
+	router.Run(":" + port)
 
 }
