1cfbda8019148f088b7f75b03b25ece6d11f818d
Marlon Vinicius Batista <marlon.batista18@hotmail.com>
2023-04-29 12:46:36 -0300

    Alter routes

--- doc-center-api/api/routes/routes.go
+++ doc-center-api/api/routes/routes.go
@@ -21,6 +21,34 @@ func ConfigRotas() *gin.Engine{
 		users.GET("/", controllers.GetAllUsers)
 		users.GET("/:id", controllers.GetUserByID)
 		users.PUT("/", controllers.UpdateUser)
+		users.GET("/:id/permissions", controllers.GetUserPermission)
+	}
+	files := routers.Group("files")
+	{
+		files.GET("/", controllers.GetAllFiles)
+		files.GET("/:id", controllers.GetFileById)
+		files.POST("/", controllers.CreateFile)
+		files.PUT("/", controllers.UpdateFile)
+		files.DELETE("/:id", controllers.DeleteFile)
+	}
+
+	permissions := routers.Group("permissions")
+	{
+		permissions.GET("/", controllers.GetAllPermissions)
+		permissions.GET("owner/:id", controllers.GetOwnerPermission)
+		permissions.GET("guest/:id", controllers.GetGuestPermission)
+		permissions.POST("/", controllers.CreatePermission)
+		permissions.PUT("/", controllers.UpdatePermission)
+		permissions.DELETE("/:id", controllers.DeletePermission)
+	}
+
+	dependents := routers.Group("dependents")
+	{
+		dependents.GET("/", controllers.GetAllDependents)
+		dependents.GET("/:id", controllers.GetDependentById)
+		// dependents.POST("/", controllers.CreateDependent)
+		// dependents.PUT("/", controllers.UpdateDependent)
+		// dependents.DELETE("/:id", controllers.DeleteDependent)
 	}
 	return routers
 }
