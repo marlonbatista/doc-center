d3634c0e7372d30e2485a996dfa4722771ea826e
nicolasbalduino <nicolasbalduino@outlook.com>
2023-05-05 20:23:11 -0300

    retirando atributos que causavam erros

--- doc-center-web/src/App.js
+++ doc-center-web/src/App.js
@@ -11,20 +11,22 @@ function App() {
           <samp className="login-form-title">Doc center</samp>
         <div className="wrap-input">
           <input
-           className={email !== "" ? 'has-val input' : 'input'}
+          //  className={email !== "" ? 'has-val input' : 'input'}
+          className="input"
            type="email"
-           value={email}
-           onChange={(e) => setEmail(e.target.value)}
+          //  value={email}
+          //  onChange={(e) => setEmail(e.target.value)}
           />
           <span className="focus-input" data-placeholder="Email"></span>
 
         </div>
         <div className="wrap-input">
           <input 
-          className={password !== "" ? 'has-val input' : "input"}
+          // className={password !== "" ? 'has-val input' : "input"}
+          className="input"
            type="password"
-           value={password}
-           onChange={(e) => setPassword(e.target.value)}
+          //  value={password}
+          //  onChange={(e) => setPassword(e.target.value)}
           
           />
           <span className="focus-input" data-placeholder="Password"></span>
