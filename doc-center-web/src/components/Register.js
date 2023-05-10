d051d9d5baba42b327dc81ce5621a1f7b65a7d35
unknown <wagner.lima.andrade@alumni.usp.br>
2023-05-10 09:48:01 -0300

    adaptando para react material. Login e register

--- doc-center-web/src/components/Register.js
+++ doc-center-web/src/components/Register.js
@@ -1,235 +1,152 @@
 import React, { useState, useRef } from "react";
-import Form from "react-validation/build/form";
-import Input from "react-validation/build/input";
-import CheckButton from "react-validation/build/button";
-import { isEmail } from "validator";
-
+import {
+TextField,
+Button,
+Radio,
+RadioGroup,
+FormControlLabel,
+FormHelperText
+} from '@material-ui/core';
 import AuthService from "../services/auth.service";
 
+const Register = () => {
+const form = useRef();
+const checkBtn = useRef();
+
+const [username, setUsername] = useState("");
+const [cpf, setCpf] = useState("");
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const [isPerson, setIsPerson] = useState(true);
+const [successful, setSuccessful] = useState(false);
+const [message, setMessage] = useState("");
+
 const required = (value) => {
-  if (!value) {
-    return (
-      <div className="alert alert-danger" role="alert">
-        This field is required!
-      </div>
-    );
-  }
+if (!value) {
+return 'Este campo é obrigatório!';
+}
 };
 
 const validEmail = (value) => {
-  if (!isEmail(value)) {
-    return (
-      <div className="alert alert-danger" role="alert">
-        This is not a valid email.
-      </div>
-    );
-  }
+if (!/^[^\s@]+@[^\s@]+.[^\s@]+$/.test(value)) {
+return 'Insira um e-mail válido!';
+}
 };
 
 const vusername = (value) => {
-  if (value.length < 3 || value.length > 20) {
-    return (
-      <div className="alert alert-danger" role="alert">
-        The username must be between 3 and 20 characters.
-      </div>
-    );
-  }
+if (value.length < 3 || value.length > 20) {
+return 'O nome de usuário deve ter entre 3 e 20 caracteres.';
+}
 };
 
 const vpassword = (value) => {
-  if (value.length < 6 || value.length > 40) {
-    return (
-      <div className="alert alert-danger" role="alert">
-        The password must be between 6 and 40 characters.
-      </div>
-    );
-  }
+if (value.length < 6 || value.length > 40) {
+return 'A senha deve ter entre 6 e 40 caracteres.';
+}
 };
 
-const Register = () => {
-  const form = useRef();
-  const checkBtn = useRef();
+const onChangeUsername = (e) => {
+const username = e.target.value;
+setUsername(username);
+};
 
-  const [username, setUsername] = useState("");
-  const [cpf, setCpf] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [isPerson, setIsPerson] = useState(true);
-  const [successful, setSuccessful] = useState(false);
-  const [message, setMessage] = useState("");
+const onChangeCpf = (e) => {
+const cpf = e.target.value;
+setCpf(cpf);
+};
 
+const onChangeEmail = (e) => {
+const email = e.target.value;
+setEmail(email);
+};
 
-  const onChangeUsername = (e) => {
-    const username = e.target.value;
-    setUsername(username);
-  };
+const onChangePassword = (e) => {
+const password = e.target.value;
+setPassword(password);
+};
 
-  const onChangeCpf = (e) => {
-    const cpf = e.target.value;
-    setCpf(cpf);
-  };
+const handleRegister = (e) => {
+e.preventDefault();
 
-  const onChangeEmail = (e) => {
-    const email = e.target.value;
-    setEmail(email);
-  };
+setMessage("");
+setSuccessful(false);
 
-  const onChangePassword = (e) => {
-    const password = e.target.value;
-    setPassword(password);
-  };
+form.current.reportValidity();
 
+if (checkBtn.current.context._errors.length === 0) {
+  AuthService.register(username, cpf, email, password, isPerson).then(
+    (response) => {
+      setMessage(response.data.message);
+      setSuccessful(true);
+    },
+    (error) => {
+      const resMessage =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
 
-  const handleRegister = (e) => {
-    e.preventDefault();
-
-    setMessage("");
-    setSuccessful(false);
-
-    form.current.validateAll();
-
-    if (checkBtn.current.context._errors.length === 0) {
-      AuthService.register(username, cpf, email, password, isPerson).then(
-        (response) => {
-          setMessage(response.data.message);
-          setSuccessful(true);
-        },
-        (error) => {
-          const resMessage =
-            (error.response &&
-              error.response.data &&
-              error.response.data.message) ||
-            error.message ||
-            error.toString();
-
-          setMessage(resMessage);
-          setSuccessful(false);
-        }
-      );
+      setMessage(resMessage);
+      setSuccessful(false);
     }
-  };
-
-  return (
-    <div className="col-md-12">
-      <div className="card card-container">
-        <img
-          src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-          alt="profile-img"
-          className="profile-img-card"
-        />
-
-        <Form onSubmit={handleRegister} ref={form}>
-          {!successful && (
-            <div>
-              <div className="form-group">
-                <label htmlFor="username">Username</label>
-                <Input
-                  type="text"
-                  className="form-control"
-                  name="username"
-                  value={username}
-                  onChange={onChangeUsername}
-                  validations={[required, vusername]}
-                />
-              </div>
-
-
-               <div className="form-group">
-          
-              <label htmlFor="cpf">CPF</label>
-              <Input
-              type="text"
-              className="form-control"
-              name="cpf"
-              value={cpf}
-              onChange={onChangeCpf}
-              validations={[required]}
-                />
-              </div>
-
-              
-              <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <Input
-                  type="text"
-                  className="form-control"
-                  name="email"
-                  value={email}
-                  onChange={onChangeEmail}
-                  validations={[required, validEmail]}
-                />
-              </div>
-
-              <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <Input
-                  type="password"
-                  className="form-control"
-                  name="password"
-                  value={password}
-                  onChange={onChangePassword}
-                  validations={[required, vpassword]}
-                />
-              </div>
-              <div className="form-group">
-              <label htmlFor="isPerson">Tipo de pessoa </label>
-            <div>
-              <div className="form-check form-check-inline">
-                <input
-                  className="form-check-input"
-                  type="radio"
-                  name="isPessoaFisica"
-                  id="isPessoaFisica-fisica"
-                  value="true"
-                  checked={isPerson}
-                  onChange={() => setIsPerson(true)}
-                />
-                <label className="form-check-label" htmlFor="isPessoaFisica-fisica">
-                  Física
-                </label>
-              </div>
-              <div className="form-check form-check-inline">
-                <input
-                  className="form-check-input"
-                  type="radio"
-                  name="isPessoaFisica"
-                  id="isPessoaFisica-juridica"
-                  value="false"
-                  checked={!isPerson}
-                  onChange={() => setIsPerson(false)}
-                />
-                <label className="form-check-label" htmlFor="isPessoaFisica-juridica">
-                  Jurídica
-                </label>
-              </div>
-            </div>
-            </div>
-
-
-              <div className="form-group">
-                <button className="btn btn-primary btn-block">Sign Up</button>
-              </div>
-            </div>
-          )}
-
-
-          {message && (
-            <div className="form-group">
-              <div
-                className={
-                  successful ? "alert alert-success" : "alert alert-danger"
-                }
-                role="alert"
-              >
-                {message}
-              </div>
-            </div>
-          )}
-          <CheckButton style={{ display: "none" }} ref={checkBtn} />
-        </Form>
-      </div>
-    </div>
   );
+}
 };
 
-export default Register;
+return (
+  <div className="col-md-12">
+    <div className="card card-container">
+      <img
+        src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+        alt="profile-img"
+        className="profile-img-card"
+      />
+      <Form onSubmit={handleRegister} ref={form}>
+        {!successful && (
+          <div>
+            <div className="form-group">
+              <TextField
+                fullWidth
+                label="Username"
+                name="username"
+                value={username}
+                onChange={onChangeUsername}
+                variant="outlined"
+                required
+                onBlur={(e) => form.current.reportValidity()}
+                error={form.current?.elements?.username?.validationMessage}
+                helperText={
+                  form.current?.elements?.username?.validationMessage
+                }
+                inputProps={{ maxLength: 20 }}
+              />
+            </div>
+
+            <div className="form-group">
+              <TextField
+                fullWidth
+                label="CPF"
+                name="cpf"
+                value={cpf}
+                onChange={onChangeCpf}
+                variant="outlined"
+                required
+                onBlur={(e) => form.current.reportValidity()}
+                error={form.current?.elements?.cpf?.validationMessage}
+                helperText={form.current?.elements?.cpf?.validationMessage}
+              />
+            </div>
+
+            <div className="form-group">
+              <Button variant="contained" color="primary" type="submit">
+                Register
+              </Button>
+            </div>
+          </div>
+        )}
+      </Form>
+    </div>
+  </div>
+);
+}
